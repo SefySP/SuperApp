955a6d3cfd5ba76ee5ac115292cf6aa7f6427ab1
Spyrosap <dit18016@go.uop.gr>
2021-06-05 23:17:27 +0300

    quiz functionality?

--- Scripts/quiz.js
+++ Scripts/quiz.js
@@ -1,21 +1,82 @@
-function showResults() 
+var myQuestions=[
 {
-    var q1 = document.quiz_form.Option1.value;
-    var q2 = document.quiz_form.Option2.value;
+    question:"Η γλώσσα ονομάστηκε 'C' λόγω του ότι πολλά από τα χαρακτηριστικά της προήλθαν από μια παλαιότερη γλώσσα, η οποία ονομαζόταν 'B'. :",
+    answers:{
+        a: 'True',
+        b: 'False'
+    },
+    correctAnswers: 'a',
+    type: 'radio' 
+},
 
-    var c = 0;
+{
+    question:"Επέλεξε τον λάθος διατυπωμένο τελεστή :",
+    answers:{
+        a: '+=',
+        b: '++',
+        c: '!&',
+        d: '!=',
+    },
+    correctAnswers: 'c',
+    type: 'radio' 
+},
 
-    if (q1 == "OPT2") 
-    {
-        c++;
-    }
-    if (q2 == "OPT1") 
-    {
-        c++;
-    }
+{
+    question:"Επέλεξε τους λάθους ισχυρισμούς :",
+    answers:{
+        a: 'Η C δεν διαθέτει garbage collection.',
+        b: 'Η C++ ξεκίνησε σαν προεπεξεργαστής της C.',
+        c: 'Mπορούν να οριστούν συναρτήσεις μέσα σε άλλες συναρτήσεις.',
+        d: 'Σε μία εντολή μπορεί να γίνουν παραπάνω από μια εκχωρήσεις τιμών.',
+    },
+    correctAnswers: ['a','b','c'],
+    type: 'checkbox'  
+},
 
-    if(c == 2)
-    {
-        alert("Bravo");
+{
+    question:"Ανέφερε μερικές γλώσσες που έχουν επηρεαστεί άμεσα ή έμμεσα από την C:",
+    answers:{ },
+    correctAnswers: ['Java','C++','Python'],
+    type: 'text'  
+}
+]
+
+
+var quizContainer=document.getElementById('quiz_form');
+var resultsContainer=document.getElementById('results');
+
+generateQuiz(myQuestions,quizContainer);
+
+function generateQuiz(questions,quizContainer){
+    function showQuestions(questions,quizContainer){
+        var output=[];
+        var answers;
+        output.push('<ol>');
+        for(var i=0;i<questions.length;i++)
+        {
+            answers=[];
+            if(questions[i].type=='radio'){
+                answers.push('<div class="display_down"> <ul class="no_decor">');
+                for(letter in questions[i].answers){
+                    answers.push('<li> <input type="radio" name="answers_'+ i +'" '+'value="' + letter + '" > '+question[i].answers[letter]+ '</li>');
+                }
+                answers.push('</ul> </div>');
+            }
+            else if(questions[i].type=='checkbox')
+            {
+                answers.push('<div class="display_down"> <ul class="no_decor">');
+                for(letter in questions[i].answers){
+                    answers.push('<li> <input type="checkbox" name="answers_'+ i +'" '+'value="' + letter + '" > '+question[i].answers[letter]+ '</li>');
+                }
+                answers.push('</ul> </div>');
+            }
+            else{
+                answers.push('<input type="text" name="answers_'+ i +'" '+'value="' + letter + '" class="text_field" style="display: inline;" autocomplete="off"> ');
+            }
+            output.push('<li>'+ questions[i].question + answers.join('') +'</li>');
+        }
+        output.push('</ol>');
+        quizContainer.innerHTML=output.join('');
     }
+    showQuestions(questions,quizContainer);
 }
