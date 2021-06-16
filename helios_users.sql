4b6842313e43a053d0544b9335c0c3b4921c7996
Spyrosap <dit18016@go.uop.gr>
2021-06-16 20:35:41 +0300

    sql

--- helios_users.sql
+++ helios_users.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 10, 2021 at 08:52 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
+-- Εξυπηρετητής: 127.0.0.1
+-- Χρόνος δημιουργίας: 16 Ιουν 2021 στις 19:35:00
+-- Έκδοση διακομιστή: 10.4.19-MariaDB
+-- Έκδοση PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `helios_users`
+-- Βάση δεδομένων: `helios_users`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `helios_users`
+-- Δομή πίνακα για τον πίνακα `helios_users`
 --
 
 CREATE TABLE `helios_users` (
@@ -41,19 +41,21 @@ CREATE TABLE `helios_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `helios_users`
+-- Άδειασμα δεδομένων του πίνακα `helios_users`
 --
 
 INSERT INTO `helios_users` (`id`, `Fname`, `Lname`, `Email`, `Username`, `Password`, `Gender`, `Date`, `Photo`, `Role`) VALUES
 (12, 'u1', 'u1', 'u1@u1.com', 'u1', '$2y$10$7bSps1OI2kgAwQxXcktCQuX7q/ULin1H2pfmydQdff0WXEK65nFYC', 'female', '2021-06-24', '../Media/u1lecpain.jpg', 'user'),
-(13, 'u2', 'u2', 'u2@u2.com', 'u2', '$2y$10$i8pB25xLCABsmiRXA50pAO.Xk.cc3AND3WIWq9WD4kAWzJwRneI72', 'male', '2021-06-24', '../Media/u2yak.jpg', 'user');
+(13, 'u2', 'u2', 'u2@u2.com', 'u2', '$2y$10$i8pB25xLCABsmiRXA50pAO.Xk.cc3AND3WIWq9WD4kAWzJwRneI72', 'male', '2021-06-24', '../Media/u2yak.jpg', 'user'),
+(14, 'dannis', 'dannis', 'dannis@gmail.com', 'dannis', '$2y$10$eyVESixKWOZm0W3cxowUtOdOu49c/b1mHyaP7.8PGyj98bfztSHuW', 'male', '2021-06-01', '../Media/dannisPgi1E6.jpg', 'user'),
+(15, '11', '11', 'f11@gmail.com', '11', '$2y$10$SqqVYFP84e0vEIDGqxkLKudlVa/QuIbmoRZAfIEt32vDobfGgIy.e', 'female', '2021-06-15', '../Media/11224441.jpg', 'user');
 
 --
--- Indexes for dumped tables
+-- Ευρετήρια για άχρηστους πίνακες
 --
 
 --
--- Indexes for table `helios_users`
+-- Ευρετήρια για πίνακα `helios_users`
 --
 ALTER TABLE `helios_users`
   ADD PRIMARY KEY (`id`),
@@ -61,14 +63,14 @@ ALTER TABLE `helios_users`
   ADD UNIQUE KEY `Username` (`Username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT για άχρηστους πίνακες
 --
 
 --
--- AUTO_INCREMENT for table `helios_users`
+-- AUTO_INCREMENT για πίνακα `helios_users`
 --
 ALTER TABLE `helios_users`
-  MODIFY `id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
